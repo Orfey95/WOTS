2bec3273eb4c9a2a7f8a22150996e700a289214b
Frolov-Viacheslav <frolov_vyacheslav21@mail.ru>
2017-11-08 19:35:31 +0200

    Update KeyPairGeneration.java

--- KeyPairGeneration.java
+++ KeyPairGeneration.java
@@ -3,11 +3,13 @@ import java.security.NoSuchAlgorithmException;
 
 public class KeyPairGeneration {
     Binarylog bl = new Binarylog();
-    PRG random = new PRG();
-
+    PRG prg = new PRG();
+    MD5 md5 = new MD5();
     int s = 128; //hash-size
     int w = 2;
     int t;
+    String X = "";
+    String Y = "";
 
     public void calculateLengths(){
         int t1 = (int) Math.ceil(s + w);
@@ -16,14 +18,25 @@ public class KeyPairGeneration {
     }
 
 
-    public String generatePrivateKey() {
-        PRG prg = new PRG();
+    public void generatePairKey() {
         calculateLengths();
-        String X = "";
+        String Xi = "";
+        String Yi = "";
+
         for(int i=0;i<t;i++){
-            X += prg.Random128();
+            Xi = prg.Random128();
+            X += Xi;
+            Yi = CalculateYi(Xi,Yi);
+            Y += Yi;
         }
-        return X;
+    }
+
+    private String CalculateYi(String Xi, String Yi){
+        Yi = Xi;
+        for(int i=1; i<Math.pow(2,w)-1;i++){
+            Yi = md5.md5Custom(Yi);
+        }
+        return Yi;
     }
 
 }
