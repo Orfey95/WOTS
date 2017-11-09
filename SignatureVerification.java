869b5b4154b3351640e895b5dc9c6f4aebc3c9a9
Frolov-Viacheslav <frolov_vyacheslav21@mail.ru>
2017-11-09 23:23:09 +0200

    Update SignatureVerification.java

--- SignatureVerification.java
+++ SignatureVerification.java
@@ -1,25 +1,26 @@
-import java.util.Arrays;
 public class SignatureVerification {
-    MD5 md5 = new MD5();
+    MD5Binary md5B = new MD5Binary();
+    MD5HEX md5H = new MD5HEX();
     SignatureGeneration sigGen = new SignatureGeneration();
     String sig = "";
 
     public boolean verifySignature(String SIGNATURE, String Message, Integer s, Integer w) {
         Integer[]b = sigGen.messagePlusCheckSum(Message, s, w);
-        System.out.println("Full Array = " + Arrays.toString(sigGen.messagePlusCheckSum(Message, s, w)));
+        //System.out.println("Full Array = " + Arrays.toString(sigGen.messagePlusCheckSum(Message, s, w)));
         String SIGNATUREi = "";
         for (int i = 0; i < KeyPairGeneration.t; i++) {
             SIGNATUREi = SIGNATURE.substring(i * s, i * s + s); // нахождение подстроки с длиной в s символ
             sig += calculateSignatureI(SIGNATUREi,b[i],w);
         }
-        if(md5.md5Custom(KeyPairGeneration.Y).compareTo(md5.md5Custom(sig)) == 0)
+        sig = md5H.md5Custom(sig);
+        if(KeyPairGeneration.Y.compareTo(sig) == 0)
             return true;
         else
             return false;
     }
     private String calculateSignatureI(String sigi, Integer bi, Integer w) {
         for (int i = 1; i <= Math.pow(2, w) - 1 - bi; i++) {
-            sigi = md5.md5Custom(sigi);
+            sigi = md5B.md5Custom(sigi);
         }
         return sigi;
     }
