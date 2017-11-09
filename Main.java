b021c107e486ec91bef38ddebe4976c114e4cc21
Sasha <sasha7692@gmail.com>
2017-11-09 20:25:11 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,22 @@
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 
+    static String Message = "11010101100011100011010110000010101011111010100110010000010000001110001001111011100100101011000100111100100011110010001010000000";
+    static int s = Message.length();
+    static int w = 3;
     public static void main(String[] args) throws NoSuchAlgorithmException {
-       KeyPairGeneration gen = new KeyPairGeneration();
-        gen.generatePairKey();
-        System.out.println("X: " + gen.X + "\n Y: " + gen.Y);
+        KeyPairGeneration gen = new KeyPairGeneration();
+        SignatureGeneration sg = new SignatureGeneration();
+        Binarylog bl = new Binarylog();
+        MD5 md5 = new MD5();
+        gen.generatePairKey(s, w);
+        System.out.println("t: " + gen.t);
+        System.out.println("X: " + gen.X + "\nY: " + gen.Y);
+        System.out.println("Full message = " + sg.messageAddZeros(Message, s, w));
+        System.out.println("Array of blocks of message = " + Arrays.toString(sg.messageSeparate(Message, s, w)));
+        System.out.println("Array of blocks of CheckSum = " + Arrays.toString(sg.checkSumSeparate(s, w)));
     }
+
 }
