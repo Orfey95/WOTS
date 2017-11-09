350d5d67d607b80785d74b4b3731cc81fffa52e4
Sasha <sasha7692@gmail.com>
2017-11-09 20:25:50 +0200

    Update Binarylog.java

--- Binarylog.java
+++ Binarylog.java
@@ -1,11 +1,6 @@
 public class Binarylog {
-    public static int binlog( int bits ) // returns 0 for bits=0
-    {
-        int log = 0;
-        if( ( bits & 0xffff0000 ) != 0 ) { bits >>>= 16; log = 16; }
-        if( bits >= 256 ) { bits >>>= 8; log += 8; }
-        if( bits >= 16  ) { bits >>>= 4; log += 4; }
-        if( bits >= 4   ) { bits >>>= 2; log += 2; }
-        return log + ( bits >>> 1 );
+    public static Double binlog(Double value){
+        value = Math.log10(value) / Math.log10(2.);
+        return value;
     }
 }
