80a51229660cd5b10fc51dad558e5f6d65f885bd
Frolov-Viacheslav <frolov_vyacheslav21@mail.ru>
2017-11-09 23:20:53 +0200

    Update KeyPairGeneration.java

--- KeyPairGeneration.java
+++ KeyPairGeneration.java
@@ -1,7 +1,8 @@
 public class KeyPairGeneration {
     Binarylog bl = new Binarylog();
     PRG prg = new PRG();
-    MD5 md5 = new MD5();
+    MD5Binary md5B = new MD5Binary();
+    MD5HEX md5H = new MD5HEX();
     static int t;
     static int t1;
     static int t2;
@@ -26,12 +27,13 @@ public class KeyPairGeneration {
             Yi = calculateYi(Xi, Yi, w);
             Y += Yi;
         }
+        Y = md5H.md5Custom(Y);
     }
 
     private String calculateYi(String Xi, String Yi, Integer w){
         Yi = Xi;
         for(int i = 1; i <= Math.pow(2, w) - 1; i++){
-            Yi = md5.md5Custom(Yi);
+            Yi = md5B.md5Custom(Yi);
         }
         return Yi;
     }
