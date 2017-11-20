9b7909983ea40d4691a2009abfe112e6042fbbaa
Sasha <sasha7692@gmail.com>
2017-11-20 10:02:21 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Scanner;
 import Config.*;
 import WOTS_CR.*;
@@ -6,7 +7,7 @@ import WOTS_PLUS.*;
 
 public class Main {
     public static void main(String[] args) throws NoSuchAlgorithmException {
-        /*MD5Binary md5b = new MD5Binary();
+        MD5Binary md5b = new MD5Binary();
 
         Scanner inMessage = new Scanner(System.in);
         System.out.printf("Input your message:\n");
@@ -16,49 +17,100 @@ public class Main {
         System.out.printf("Input parametr w:\n");
         int w = Integer.parseInt(inW.nextLine());
 
+        Scanner inChoose = new Scanner(System.in);
+        System.out.printf("Choose algoritm (1 - WOTS, 2 - WOTS+):\n");
+        int chooseAlgoritm = Integer.parseInt(inW.nextLine());
+
         String Message = "";
         int s = 0;
         Message = md5b.md5Custom(MessageOrigin);
         s = Message.length();
-        KeyPairGeneration kpg = new KeyPairGeneration();
-        SignatureGeneration sg = new SignatureGeneration();
         Binarylog bl = new Binarylog();
-        SignatureVerification sv = new SignatureVerification();
-        kpg.generatePairKey(s, w);
-        System.out.println("X - Private key; Y - Public key; Y` - Validation string.\n");
-        System.out.println("X : " + kpg.X + "\nY : " + kpg.Y);
-        sg.generateSignature(Message, s, w);
-        System.out.println("Signature: " + sg.SIGNATURE);
-
-        boolean equalSignature = false;
-        if(sv.verifySignature(sg.SIGNATURE, Message, s,w)) {
-            equalSignature = true;
+        if(chooseAlgoritm > 2){
+            System.out.println("Wrong input!");
         }
+        if(chooseAlgoritm == 1) {
+            KeyPairGeneration kpg = new KeyPairGeneration();
+            SignatureGeneration sg = new SignatureGeneration();
 
-        System.out.println("Y`: " + sv.sig);
+            SignatureVerification sv = new SignatureVerification();
+            long start = System.currentTimeMillis(); // STAR TIMER
+            kpg.generatePairKey(s, w);
+            System.out.println("X - Private key; Y - Public key; Y` - Validation string.\n");
+            System.out.println("X : " + kpg.X + "\nY : " + kpg.Y);
+            sg.generateSignature(Message, s, w);
+            System.out.println("Signature: " + sg.SIGNATURE);
 
-        if(equalSignature == true){
-            System.out.println("\nThe signature is valid!");
+            boolean equalSignature = false;
+            if (sv.verifySignature(sg.SIGNATURE, Message, s, w)) {
+                equalSignature = true;
+            }
+            long finish = System.currentTimeMillis(); // FINISH TIMER
+            System.out.println("Y`: " + sv.sig);
+            long timeConsumedMillis = finish - start;
+            //Statistic
+            System.out.println("\n" + "Statictic:");
+            System.out.println("Length of Private key: " + KeyPairGeneration.X.length() + " bits");
+            System.out.println("Length of Signature: " + sg.SIGNATURE.length() + " bits");
+            System.out.println("Time: " + timeConsumedMillis + "ms");
+            //Statistic
+
+            if (equalSignature == true) {
+                System.out.println("\nThe signature is valid!");
+            } else {
+                System.out.println("The signature is NOT valid!");
+            }
         }
-        else {
-            System.out.println("The signature is NOT valid!");
-        }*/
-        int s = 128;
-        int w = 3;
-        MD5Binary md5b = new MD5Binary();
-        String Message = md5b.md5Custom("aaa");
-        P_KeyPairGeneration p_kpg = new P_KeyPairGeneration();
-        P_SignatureGeneration p_sg = new P_SignatureGeneration();
-        P_SignatureVerification p_sv = new P_SignatureVerification();
-        p_kpg.calculateSK(s,w);
-        p_kpg.calculatePK(s,w);
-        p_sg.generateSignature(Message, s, w);
-        p_sv.verifySignature(p_sg.SIGNATURE, Message, s, w);
+        if(chooseAlgoritm == 2) {
+        /*String Message = "00110000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001010";
+        int s = Message.length();
+        int w = 3;*/
+
+            P_KeyPairGeneration p_kpg = new P_KeyPairGeneration();
+            P_SignatureGeneration p_sg = new P_SignatureGeneration();
+
+        /*p_kpg.calculateSK(s,w);
+        p_kpg.calculatePK(s, w);
         System.out.println("l: " + String.valueOf(P_KeyPairGeneration.l));
         System.out.println("X: " + P_KeyPairGeneration.X);
         System.out.println("Y: " + P_KeyPairGeneration.Y);
-        System.out.println("r: " + P_KeyPairGeneration.r);
-        System.out.println("SIGNATURE: " + p_sg.SIGNATURE);
-        System.out.println("sig: " + p_sv.sig);
+        System.out.println("r: " + P_KeyPairGeneration.r);*/
+            p_kpg.calculateLengths(s, w);
+            //System.out.println(p_sg.messageAddZeros(Message, s, w));
+            //System.out.println(Arrays.asList(p_sg.messageSeparate(Message, s, w)));
+            //System.out.println(p_sg.checkSumAddZeros(p_sg.CBinary, s, w));
+            //System.out.println(Arrays.asList(p_sg.checkSumSeparate(s, w)));
+            System.out.println(Arrays.asList(p_sg.messagePlusCheckSum(Message, s, w)));
+        }
+        Scanner inExit = new Scanner(System.in);
+        System.out.printf("Exit or not? (0 - Exit, 1 - Start):\n");
+        int exitOrNot = Integer.parseInt(inExit.nextLine());
+        if(exitOrNot == 1){
+            Reboot();
+            main(args);
+        }
+    }
+
+    public static void Reboot(){ // Reboot global parametrs
+        SignatureGeneration sg = new SignatureGeneration();
+        SignatureVerification sv = new SignatureVerification();
+        KeyPairGeneration.t = 0;
+        KeyPairGeneration.t1 = 0;
+        KeyPairGeneration.t2 = 0;
+        KeyPairGeneration.X = "";
+        KeyPairGeneration.Y = "";
+        sg.CBinary = "";
+        sg.SIGNATURE = "";
+        sv.sig = "";
+        P_SignatureGeneration p_sg = new P_SignatureGeneration();
+        P_SignatureVerification p_sv = new P_SignatureVerification();
+        P_KeyPairGeneration.l = 0;
+        P_KeyPairGeneration.l1 = 0;
+        P_KeyPairGeneration.l2 = 0;
+        P_KeyPairGeneration.X = "";
+        P_KeyPairGeneration.Y = "";
+        p_sg.CBinary = "";
+        p_sg.SIGNATURE = "";
+        p_sv.sig = "";
     }
 }
